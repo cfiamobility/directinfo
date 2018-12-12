0f3d94d9ca94760dd14465c2f1b671880fb80a0f
boskovicm <milos.boskovic@canada.ca>
2018-12-12 16:51:30 -0500

    fixed bug where csv file would not download correctly

--- app/src/main/java/ca/gc/inspection/directinfo/DownloadDatabase.java
+++ app/src/main/java/ca/gc/inspection/directinfo/DownloadDatabase.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -25,6 +26,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -54,10 +58,10 @@ public class DownloadDatabase extends Activity {
         progressBar = findViewById(R.id.SpinKit);
         progressBar.setIndeterminateDrawable(wave);
         context = getApplicationContext();
-//        dbHelper = new DirectInfoDbHelper(context);
-//        db = dbHelper.getWritableDatabase();
-//        db.execSQL(DirectInfo.SQL_DELETE_ENTRIES);
-//        db.execSQL(DirectInfo.SQL_CREATE_ENTRIES);
+        dbHelper = new DirectInfoDbHelper(context);
+        db = dbHelper.getWritableDatabase();
+        db.execSQL(DirectInfo.SQL_DELETE_ENTRIES);
+        db.execSQL(DirectInfo.SQL_CREATE_ENTRIES);
 
         // animation
         logo = findViewById(R.id.logo);
@@ -77,57 +81,58 @@ public class DownloadDatabase extends Activity {
             Log.d(TAG, "doInBackground: come into doInBackGround");
             try {
 
-                dbHelper = new DirectInfoDbHelper(context);
-                db = dbHelper.getWritableDatabase();
-                db.execSQL(DirectInfo.SQL_DELETE_ENTRIES);
-                db.execSQL(DirectInfo.SQL_CREATE_ENTRIES);
-
                 gedsOpenData = new File(getApplicationContext().getFilesDir() + "/directinfo.csv");
                 gedsOpenData.createNewFile();
 
                 URL u = new URL(urls[0]);
                 URLConnection conn = u.openConnection();
+                conn.setRequestProperty("Content-Type", "text/plain; charset=UTF-8");
+                conn.setDoInput(true);
+                conn.connect();
 
                 int contentLength = conn.getContentLength();
-                DataInputStream stream = new DataInputStream(u.openStream());
+                Log.d(TAG, "doInBackground: contentLength: " + contentLength);
 
+                InputStream stream = conn.getInputStream();
+                FileOutputStream fos = new FileOutputStream(gedsOpenData);
+                int downloadSize = 0;
 
-                byte[] buffer = new byte[contentLength];
-                stream.readFully(buffer);
-                stream.close();
+                byte[] buffer = new byte[1024];
+                int bufferLength;
+                while ((bufferLength = stream.read(buffer)) > 0) {
+                    fos.write(buffer, 0, bufferLength);
+                    downloadSize += bufferLength;
+                }
 
-                DataOutputStream fos = new DataOutputStream(new FileOutputStream(gedsOpenData));
-                fos.write(buffer);
-                fos.flush();
                 fos.close();
-
+                stream.close();
+                Log.d(TAG, "doInBackground: downloadSize: " + downloadSize);
 
             } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                e.getMessage();
+                Log.d(TAG, "doInBackground: ERROR: FileNotFoundException: " + e.getMessage());
             } catch (IOException e) {
-                e.getMessage();
+                Log.d(TAG, "doInBackground: ERROR: IOException: " + e.getMessage());
+
             }
-
-
 
             return null;
         }
 
         @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
         protected void onPostExecute(Void aVoid) {
-            db.beginTransaction();
+            populateDatabase();
+        }
+    }
 
-            CsvParserSettings settings = new CsvParserSettings();
-            settings.setNumberOfRowsToSkip(1); // skips the first row of the file as it contains the column names
-            settings.setSkipEmptyLines(true);
 
-            CsvParser parser = new CsvParser(settings);
+    public void populateDatabase(){
+        db.beginTransaction();
+
+        CsvParserSettings settings = new CsvParserSettings();
+        settings.setNumberOfRowsToSkip(1); // skips the first row of the file as it contains the column names
+        settings.setSkipEmptyLines(true);
+
+        CsvParser parser = new CsvParser(settings);
 
         /*
         The downloaded CSV file is encoded in ANSI which did not process properly with the parser used.
@@ -136,51 +141,50 @@ public class DownloadDatabase extends Activity {
         Oracle website @ https://docs.oracle.com/javase/8/docs/technotes/guides/intl/encoding.doc.html
         This encoding now shows the names and addresses with the proper format.
         */
-            try {
-                parser.beginParsing(gedsOpenData, "Cp1252");
-                String[] row;
-                while ((row = parser.parseNext()) != null) {
+        try {
+            parser.beginParsing(gedsOpenData, "Cp1252");
+            String[] row;
+            while ((row = parser.parseNext()) != null) {
 
-                    ContentValues values = new ContentValues();
-                    values.put(DirectInfo.COLUMN_NAME_FIRST_NAME, row[0]);
-                    values.put(DirectInfo.COLUMN_NAME_LAST_NAME, row[1]);
-                    values.put(DirectInfo.COLUMN_NAME_PREFIX_EN, row[2]);
-                    values.put(DirectInfo.COLUMN_NAME_TITLE_EN, row[3]);
-                    values.put(DirectInfo.COLUMN_NAME_TELEPHONE_NUMBER, row[4]);
-                    values.put(DirectInfo.COLUMN_NAME_MOBILE_NUMBER, row[5]);
-                    values.put(DirectInfo.COLUMN_NAME_EMAIL, row[6]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_STREET_NUMBER, row[7]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_STREET_NAME, row[8]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_BUILDING_UNIT_TYPE, row[9]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_BUILDING_UNIT_ID, row[10]);
-                    values.put(DirectInfo.COLUMN_NAME_PO_BOX_EN, row[11]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_CITY_EN, row[12]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_PROVINCE_EN, row[13]);
-                    values.put(DirectInfo.COLUMN_NAME_C, row[14]);
-                    values.put(DirectInfo.COLUMN_NAME_POSTAL_CODE, row[15]);
-                    values.put(DirectInfo.COLUMN_NAME_BUILDING_NAME_EN, row[16]);
-                    values.put(DirectInfo.COLUMN_NAME_FLOOR, row[17]);
-                    values.put(DirectInfo.COLUMN_NAME_ROOM, row[18]);
-                    values.put(DirectInfo.COLUMN_NAME_PHYSICAL_STREET_NUMBER, row[19]);
-                    values.put(DirectInfo.COLUMN_NAME_PHYSICAL_STREET_NAME, row[20]);
-                    values.put(DirectInfo.COLUMN_NAME_PHYSICAL_BUILDING_UNIT_TYPE, row[21]);
-                    values.put(DirectInfo.COLUMN_NAME_PHYSICAL_BUILDING_UNIT_ID, row[22]);
-                    values.put(DirectInfo.COLUMN_NAME_PHYSICAL_CITY_EN, row[23]);
-                    values.put(DirectInfo.COLUMN_NAME_PHYSICAL_PROVINCE_EN, row[24]);
-                    db.insert(DirectInfo.TABLE_NAME, null, values);
-                }
-
-            } catch (IllegalArgumentException e) {
-                Log.d(TAG, "doInBackground: ERROR: " + e.getMessage());
-            } finally {
-                parser.stopParsing();
+                ContentValues values = new ContentValues();
+                values.put(DirectInfo.COLUMN_NAME_FIRST_NAME, row[0]);
+                values.put(DirectInfo.COLUMN_NAME_LAST_NAME, row[1]);
+                values.put(DirectInfo.COLUMN_NAME_PREFIX_EN, row[2]);
+                values.put(DirectInfo.COLUMN_NAME_TITLE_EN, row[3]);
+                values.put(DirectInfo.COLUMN_NAME_TELEPHONE_NUMBER, row[4]);
+                values.put(DirectInfo.COLUMN_NAME_MOBILE_NUMBER, row[5]);
+                values.put(DirectInfo.COLUMN_NAME_EMAIL, row[6]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_STREET_NUMBER, row[7]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_STREET_NAME, row[8]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_BUILDING_UNIT_TYPE, row[9]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_BUILDING_UNIT_ID, row[10]);
+                values.put(DirectInfo.COLUMN_NAME_PO_BOX_EN, row[11]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_CITY_EN, row[12]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_PROVINCE_EN, row[13]);
+                values.put(DirectInfo.COLUMN_NAME_C, row[14]);
+                values.put(DirectInfo.COLUMN_NAME_POSTAL_CODE, row[15]);
+                values.put(DirectInfo.COLUMN_NAME_BUILDING_NAME_EN, row[16]);
+                values.put(DirectInfo.COLUMN_NAME_FLOOR, row[17]);
+                values.put(DirectInfo.COLUMN_NAME_ROOM, row[18]);
+                values.put(DirectInfo.COLUMN_NAME_PHYSICAL_STREET_NUMBER, row[19]);
+                values.put(DirectInfo.COLUMN_NAME_PHYSICAL_STREET_NAME, row[20]);
+                values.put(DirectInfo.COLUMN_NAME_PHYSICAL_BUILDING_UNIT_TYPE, row[21]);
+                values.put(DirectInfo.COLUMN_NAME_PHYSICAL_BUILDING_UNIT_ID, row[22]);
+                values.put(DirectInfo.COLUMN_NAME_PHYSICAL_CITY_EN, row[23]);
+                values.put(DirectInfo.COLUMN_NAME_PHYSICAL_PROVINCE_EN, row[24]);
+                db.insert(DirectInfo.TABLE_NAME, null, values);
             }
 
-            db.setTransactionSuccessful();
-            db.endTransaction();
-            startActivity(new Intent(getApplicationContext(), SearchActivity.class));
-            finish();
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, "populateDatabase: ERROR: " + e.getMessage());
+        } finally {
+            parser.stopParsing();
         }
+
+        db.setTransactionSuccessful();
+        db.endTransaction();
+        startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+        finish();
     }
 
     @Override
