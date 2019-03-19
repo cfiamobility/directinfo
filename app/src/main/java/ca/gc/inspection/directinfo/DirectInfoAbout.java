d4e7c66b0686daa9937da4d12e6bcd4ed7acb45a
MendozaE <edison.mendoza@canada.ca>
2019-03-19 13:56:40 -0400

    Database update:
    Added formatNum function for displaying phone/work numbers
    Minor code clean up

--- app/src/main/java/ca/gc/inspection/directinfo/DirectInfoAbout.java
+++ app/src/main/java/ca/gc/inspection/directinfo/DirectInfoAbout.java
@@ -3,7 +3,6 @@ package ca.gc.inspection.directinfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 
 public class DirectInfoAbout extends AppCompatActivity {
